0cdc027abf2b419533c7eaec0642e469edb5418f
Sandy <sangzicao@gmail.com>
2019-03-06 17:40:53 -0800

    created patch and delete routes

--- routes/items.js
+++ routes/items.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = new express.Router();
 const items = require('../fakeDb')
-
+const ExpressError = require('../expressError');
 const users = [];
 
 /** GET /users: get list of users */
@@ -28,12 +28,38 @@ router.post("/", function(req, res, next) {
     }
 });
 
-/** DELETE /users/[id]: delete user, return status */
+/** GET a single item by its name */
 
 router.get("/:name", function(req, res, next) {
     try {
       let name = req.params.name;
       let foundItem = items.find(item => item.name === name);
+      if(foundItem === undefined){
+        throw new ExpressError("item not found",404);
+      }
+      return res.json(foundItem);
+    } catch(err) {
+        next(err)
+    }
+});
+
+/** Patch modify item by name or name and price */
+
+router.patch("/:name", function(req, res, next) {
+    try {
+      let name = req.params.name;
+      let foundItem = items.find(item => item.name === name);
+      let newName = req.body.name;
+      let newPrice = req.body.price;
+      if(foundItem === undefined){
+        throw new ExpressError("item not found",404);
+      }
+      if(newName !== undefined){
+        foundItem.name = newName;
+      }
+      if(newPrice !== undefined){
+        foundItem.price = newPrice;
+      }
       return res.json(foundItem);
     } catch(err) {
         next(err)
@@ -42,20 +68,17 @@ router.get("/:name", function(req, res, next) {
 
 /** DELETE /users/[id]: delete user, return status */
 
-router.get("/", function(req, res, next) {
+
+router.delete("/:name", function(req, res, next) {
     try {
-
-      return res.json();
-    } catch(err) {
-        next(err)
-    }
-});
-
-/** DELETE /users/[id]: delete user, return status */
-
-
-router.get("/", function(req, res, next) {
-    try {
+      let name = req.params.name;
+      let foundItem = items.find(item => item.name === name);
+      if(foundItem === undefined){
+        throw new ExpressError("item not found",404);
+      }
+      let removeIndex = items.map( item => item.name).indexOf(name);
+      let removedItem = items.splice(removeIndex,1);
+      return res.json(items);
 
     } catch(err) {
         next(err)
